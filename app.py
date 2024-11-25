1e9506706fd4464dea3b7bd78b57b34b39776fc1
Alvarokstar <afercal1704@fp.iesromerovargas.com>
2024-11-25 08:24:18 +0100

    new commit

--- app.py
+++ app.py
@@ -1,58 +1,90 @@
-from flask import Flask, request, session, render_template, flash
+from flask import Flask, request, session, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
 app.secret_key = "hola"
 
-USERNAME= 'username'
-PASSWORD= 'password'
-users = {}
-series = {}
+users = [{
+    "username: Alvaro",
+    "password: 123",
+    "films: ",
+
+}
+
+]
 
 @app.route('/')
 def home():
-    if 'logged in' in session:
+    if 'logged_in' in session:
         return render_template('home.html')
-    else:
-        return render_template('register.html')
+    return redirect(url_for('register'))
 
+@app.route('/films')
+def films():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    username = session['username']
+    user_films = users[username].get('films', [])
+    categories = {
+        "wish_list": "Films that I want to watch",
+        "watching": "Films that I'm watching",
+        "watched": "Watched"
+    }
+    categorized_films = {key: [] for key in categories.keys()}
+    for film in user_films:
+        categorized_films[film['category']].append(film)
+    return render_template('films.html', categories=categories, films=categorized_films)
+
+@app.route('/add_films', methods=['GET', 'POST'])
+def add_films():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        new_film = {
+            "name": request.form['name'],
+            "synopsis": request.form['synopsis'],
+            "score": int(request.form['score']),
+            "genre": request.form['genre'],
+            "release_date": request.form['release_date'],
+            "category": request.form['category'],
+        }
+        username = session['username']
+        users[username]['films'].append(new_film)
+        return redirect(url_for('films'))
+    return render_template('add_films.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        if users.get(username):
+        if username in users:
             error = 'This username is in use. Try with another username.'
             return render_template('register.html', error=error)
-
-        users[username] = password
+        users[username] = {'password': password, 'films': []}
         flash('The username was registered successfully. Please, login.')
-        return render_template('login.html')
-
+        return redirect(url_for('login'))
     return render_template('register.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-
-        if username == USERNAME and password == PASSWORD:
+        if username in users and users[username]['password'] == password:
             session['logged_in'] = True
             session['username'] = username
-            return render_template('home.html')
+            return redirect(url_for('home'))
         else:
-            error= 'Invalid username or password'
+            error = 'Invalid username or password'
             return render_template('login.html', error=error)
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     session.pop('username', None)
-    return render_template('login.html')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
